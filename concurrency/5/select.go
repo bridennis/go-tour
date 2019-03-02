942854143c468c6b0442d5fb825e64c727e1b9b4
bridennis <5626164+bridennis@users.noreply.github.com>
2019-03-02 09:00:57 +0300

    Пример: Числа Фиббоначи (select)

---
+++ concurrency/5/select.go
@@ -0,0 +1,33 @@
+/*
+	Пример: Числа Фиббоначи (select)
+		https://go-tour-ru-ru.appspot.com/concurrency/5
+*/
+
+package main
+
+import "fmt"
+
+func fibonacci(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
+
+func main() {
+	c := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+	fibonacci(c, quit)
+}
